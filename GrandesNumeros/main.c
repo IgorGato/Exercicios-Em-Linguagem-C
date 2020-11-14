2db94f2f200ef2a58fe58710b10dba3abb97053c
Igor Gato <igormqg@gmail.com>
2020-11-14 00:04:23 -0300

    Somando numeros muito grandes utilizando listas duplamente encadeadas

--- GrandesNumeros/main.c
+++ GrandesNumeros/main.c
@@ -6,12 +6,12 @@ int main(){
    
     conta_t **inputs;
     int num_inputs;
-
+    // insira o numero de operacoes que deseja realizar
+    // depois insira na ordem: a operacao, e ambos os numeros
     scanf("%d", &num_inputs);
-    fgetc(stdin);
+    
     inputs = ler_guardar_input(num_inputs);
     escolher_operacao(inputs, num_inputs);
-
 
     return 0;
 }
